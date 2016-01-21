978cb4c53b7f6edca92dc849a3ea4e1c1069dc93
Osmozy <os_mo08@hotmail.com>
2016-01-21 21:12:37 +0300

    Started to develop the LocalizedViewService

---
+++ source/IdentityServer3.Contrib.Localization.UI/LocalizedViewService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Services.Default;
+using IdentityServer3.Core.ViewModels;
+
+namespace IdentityServer3.Contrib.Localization.UI
+{
+    public class LocalizedViewService : DefaultViewService
+    {
+        public LocalizedViewService(DefaultViewServiceOptions config, LocalizedViewLoader viewLoader) : base(config, viewLoader)
+        {
+        }
+
+        protected override Task<Stream> Render(CommonViewModel model, string page, IEnumerable<string> stylesheets, IEnumerable<string> scripts)
+        {
+
+            return base.Render(model, page, stylesheets, scripts);
+        }
+    }
+}
